bcacfe9e82ec5fb3daca15124294e954b2a6e1ba
Kyle Jeynes <40298132+Jaquarh@users.noreply.github.com>
2019-01-30 07:25:53 +0000

    V2 - added base64 encoding

--- Cipher/Cipher.php
+++ Cipher/Cipher.php
@@ -1,65 +1,78 @@
 <?php
 
+namespace Cipher;
+
 /**
  * @author: Kyle Jeynes @ Iezon Solutions <okaydots@gmail.com>
  * @copyright (c) 2019 Kyle Jeynes, All Rights Reserved.
+ * @version 1.1.2
+ * @description Container for LibSodium extension to allow for robust encryption and signing.
  */
 
-namespace Cipher\Cipher;
+use Exception;
 
 trait Cipher
 {
     /*---------------------------------------------------------------------------*/
     /*                           ENCRYPTION PKI                                  */
     /*---------------------------------------------------------------------------*/
+
     /**
      * Issues a private and public key ready for encryption and decryption between two parties.
      * @return object
-     * @throws \SodiumException
+     * @throws Exception
      */
     protected function issueKeys()
     {
         $pki = sodium_crypto_box_keypair();
+
         return (object) [
             'private' => sodium_crypto_box_secretkey($pki),
             'public' => sodium_crypto_box_publickey($pki)
         ];
     }
+
     /**
      * Encrypts a message based on a 3rd parties public key and 1st party private key.
+     * V2 uses base64 mutual translator for arrays and objects to be encrypted.
      * @param $public
      * @param $private
      * @param $message
      * @return object
-     * @throws \SodiumException
+     * @throws Exception
      */
     protected function encrypt($public, $private, $message)
     {
-        $cipher = sodium_crypto_box($message, ($nonce = random_bytes(SODIUM_CRYPTO_BOX_NONCEBYTES)), $this->keyPair($private, $public));
+        $cipher = sodium_crypto_box(base64_encode($message), ($nonce = random_bytes(SODIUM_CRYPTO_BOX_NONCEBYTES)), $this->keyPair($private, $public));
+
         return (object) [
             'cipher' => $cipher,
             'nonce'  => $nonce
         ];
     }
+
+
     /**
      * Decrypts a message based on 1st party private key and 3rd party public key and nonce.
+     * V2 uses base64 decoding.
      * @param $private
      * @param $public
      * @param $cipher
      * @param $nonce
      * @return bool|string
-     * @throws \SodiumException
+     * @throws Exception
      */
     protected function decrypt($private, $public, $cipher, $nonce)
     {
-        return sodium_crypto_box_open($cipher, $nonce, $this->keyPair($private, $public));
+        return base64_decode(sodium_crypto_box_open($cipher, $nonce, $this->keyPair($private, $public)));
     }
+
     /**
      * Generates a key pair based on private and public keys
      * @param $private
      * @param $public
      * @return string
-     * @throws \SodiumException
+     * @throws Exception
      */
     private function keyPair($private, $public)
     {
@@ -68,33 +81,38 @@ trait Cipher
             $public
         );
     }
+
     /*---------------------------------------------------------------------------*/
     /*                           SIGNATURE PKI                                   */
     /*---------------------------------------------------------------------------*/
+
     /**
      * Issues a set of signature keys.
      * @return object
-     * @throws \SodiumException
+     * @throws Exception
      */
     protected function issueSignatureKeys()
     {
         $pki = sodium_crypto_sign_keypair();
+
         return (object) [
             'private' => sodium_crypto_sign_secretkey($pki),
             'public'  => sodium_crypto_sign_publickey($pki),
         ];
     }
+
     /**
      * Sign a message using private key.
      * @param $message
      * @param $private
      * @return string
-     * @throws \SodiumException
+     * @throws Exception
      */
     protected function signMessage($message, $private)
     {
         return sodium_crypto_sign($message, $private);
     }
+
     /**
      * Verify that this come from a certain party.
      * @param $signature
